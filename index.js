d1acacbbe8cfb27a78512627ecf8e1a42b05b340
Spencer Sharpe <jspencersharpe@gmail.com>
2015-08-19 11:16:47 -0500

    installed foundation and set up styling structure

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 'use strict';
 
+var express = require('express');
+var sassCSS = require('node-sass-middleware');
 require('babel/register');
 require('./app/');
+
+var app = express();
+
+app.use(sassCSS('www/stylesheets'));
