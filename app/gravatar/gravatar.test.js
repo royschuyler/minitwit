390669af4ba45a5282d3237784486473aff1bd07
Noah Yarian <noah.yarian@gmail.com>
2015-08-19 17:09:13 -0500

    Add module to generate Gravatar URLs

---
+++ app/gravatar/gravatar.test.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var expect = require('chai').expect;
+var getGravatar = require('./gravatar');
+var gravatarNoah = 'http://www.gravatar.com/avatar/d1827cb7215433baead881bb79520dd8.jpg';
+
+describe('getGravatar()', function () {
+  it('should return the correct Gravatar URL when given a correctly formatted email address', function() {
+    var email = 'noah.yarian@gmail.com';
+    var output = getGravatar(email);
+    expect(output).to.equal(gravatarNoah);
+  });
+  it('should return the correct Gravatar URL when given an email address with leading and/or trailing whitespace', function() {
+    var email = ' noah.yarian@gmail.com ';
+    var output = getGravatar(email);
+    expect(output).to.equal(gravatarNoah);
+  });
+  it('should return the correct Gravatar URL when given an email address with uppercase letters', function() {
+    var email = 'Noah.Yarian@Gmail.com';
+    var output = getGravatar(email);
+    expect(output).to.equal(gravatarNoah);
+  });
+});
