aec30f0b2bbf7e3ccb858840302b38b0e887d87d
calebgregory <calebgregory@gmail.com>
2015-08-19 12:17:12 -0500

    Adding autocomplete_mention routes to app routes

--- app/routes.js
+++ app/routes.js
@@ -4,7 +4,10 @@ var express = require('express');
 var router = express.Router();
 
 var home = require('./home/routes');
+var autocomplete = require('./autocomplete_mention/routes');
 
 router.use('/', home);
+router.use('/autocomplete', autocomplete);
+
 
 module.exports = router;
