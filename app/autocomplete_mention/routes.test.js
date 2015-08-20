a0465d3b04a325ab381b58fe5a199a86f18ec3f7
calebgregory <calebgregory@gmail.com>
2015-08-19 22:20:38 -0500

    Restructuring tests because DRY, and making tests more specific

--- app/autocomplete_mention/routes.test.js
+++ app/autocomplete_mention/routes.test.js
@@ -7,13 +7,32 @@ var app = require('../../app');
 
 describe('Autocomplete', function() {
   describe('GET /mention', function() {
-    it('should send a response', function(done) {
+
+    before(function(done) { // add users to user collection in db
+      var users = [
+        { name : 'world' },
+        { name : 'work' },
+        { name : 'woman' },
+      ];
+      require('../../lib/mongo').connect(function(err,db) {
+        db.collection('user').drop( // but clear collection first
+          function(err,result) {
+            db.collection('user').insert( users ,
+              function(err,result) {
+                if(err) throw err;
+                done();
+              });
+          });
+      });
+    });
+
+    it('should send an empty array when there\'s no query', function(done) {
       request(app)
         .get('/autocomplete/mention')
         .expect(200)
         .end(function(err,res) {
           if(err) return done(err);
-          expect(res).to.exist;
+          expect(res.body).to.eql([]);
           done();
         });
     });
@@ -26,78 +45,36 @@ describe('Autocomplete', function() {
         .expect(200,done);
     });
 
-    var users = [
-      { name : 'world' },
-      { name : 'work' },
-      { name : 'woman' },
-    ];
-
-    it('should respond with matches', function(done) {
-      require('../../lib/mongo').connect(function(err,db) {
-        db.collection('user').insert( users ,
-          function(err,result) {
-            if(err) throw err;
-            request(app)   // seeking matches for wor
-              .get('/autocomplete/mention?pattern=wor')
-              .expect(200)
-              .end(function(err, res) {
-                if(err) return done(err);
-
-                expect(res.text).to.contain('world');
-                expect(res.text).to.contain('work');
-
-                db.collection('user').remove( {},
-                  function(err, result) {
-                    if(err) throw err;
-                    done();
-                  });
-              });
-          });
-      });
-    });
-
-    it('should not respond with not-matches', function(done) {
-      require('../../lib/mongo').connect(function(err,db) {
-        db.collection('user').insert( users ,
-          function(err,result) {
-            if(err) throw err;
-            request(app)   // seeking matches for worl
-              .get('/autocomplete/mention?pattern=worl')
-              .expect(200)
-              .end(function(err, res) {
-                if(err) return done(err);
-
-                expect(res.text).not.to.contain('work');
-
-                db.collection('user').remove( {} ,
-                  function(err, result) {
-                    if(err) throw err;
-                    done();
-                  });
-              });
-          });
+    it('should respond with matches and not with non-matches', function(done) {
+      request(app)   // seeking matches for wor
+        .get('/autocomplete/mention?pattern=wor')
+        .expect(200)
+        .end(function(err, res) {
+          if(err) return done(err);
+          expect(res.body).to.deep.equal([
+            {name: 'world'}, {name: 'work'}
+          ]);
+          done();
       });
     });
 
     it('should not have _id in response', function(done) {
+      request(app)   // seeking matches for worl
+        .get('/autocomplete/mention?pattern=worl')
+        .expect(200)
+        .end(function(err, res) {
+          if(err) return done(err);
+          expect(res.text).not.to.contain('_id');
+          done();
+        });
+    });
+
+    after(function(done) {
       require('../../lib/mongo').connect(function(err,db) {
-        db.collection('user').insert( users ,
-          function(err,result) {
+        db.collection('user').drop(
+          function(err, result) {
             if(err) throw err;
-            request(app)   // seeking matches for worl
-              .get('/autocomplete/mention?pattern=worl')
-              .expect(200)
-              .end(function(err, res) {
-                if(err) return done(err);
-
-                expect(res.text).not.to.contain('_id');
-
-                db.collection('user').remove( {} ,
-                  function(err, result) {
-                    if(err) throw err;
-                    done();
-                  });
-              });
+            done();
           });
       });
     });
