c524616742b4172bf11b87f5f629193aca0e09cd
Scott Humphries <scott@sscotth.io>
2015-08-20 15:20:56 -0500

    add additional eslint rules (fixes #33)

--- app/gravatar/gravatar.js
+++ app/gravatar/gravatar.js
@@ -3,7 +3,7 @@
 var _ = require('lodash');
 var md5 = require('md5');
 
-module.exports = function(email) {
+module.exports = function (email) {
   var url = 'http://www.gravatar.com/avatar/';
   return url + md5(_.trim(email).toLowerCase()) + '.jpg';
 };
