d9ede43f45f36188fdb5d7dcdc163f82384c8f4c
chadcates <cates.chad@gmail.com>
2015-08-20 11:25:53 -0500

    wrote test for tweet delete in delet.test.js

--- app/delete.test.js
+++ app/delete.test.js
@@ -14,17 +14,42 @@ describe('Mocha + Chai', function(){
 	});
 });
 
+
+//  delete test w/o authentication
+
 describe('delete w/o authentication', function() {
 	describe('POST /delete', function(){
 	  it('should respond with error', function(done){
 	  	request(app)
+	  	if(!user){
 	  	.post('/delete')
 	  	.expect(401)
 	  	.end(function (err, res) {
 	  		if (err) throw err;
 	  		expect(res.text).to.contain('You are not logged in');
 	  		done();
-         });
+          });
+	    }
 	  });
    });
 });
+
+//  as user A deleting a tweet from User B profile
+
+descibe('deleting another users tweet', function(){
+  describe('POST /delete/tweet_id', function(){
+  	it('should not delete the tweet', function(done){
+  		request(app)
+  		if(tweet.userID !== userA._id){
+  			.post('/delete/:id')
+  			.expect(403)
+  			.end(function (err, res){
+  				if (err) throw err;
+  				expect(res.text).to.contain('Cannot delete another users tweet');
+  				done();
+  			});
+  		 }
+      });
+   });
+});
+
