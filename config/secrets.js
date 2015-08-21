14418cbec4107e17b4d8447c3d4bf2e1904f06a4
Scott Humphries <scott@sscotth.io>
2015-08-20 21:45:34 -0500

    use different db in test env

--- config/secrets.js
+++ config/secrets.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var databaseName = 'minitwitdb';
+var databaseName = process.env.NODE_ENV === 'test' ? 'minitwitdb-test' : 'minitwitdb';
 
 module.exports = {
   db: process.env.MONGODB_URL || `mongodb://localhost:27017/${databaseName}`
