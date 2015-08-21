d3859c82a13ce9e646f60f656b97be3dce019584
calebgregory <calebgregory@gmail.com>
2015-08-21 14:20:07 -0500

    Moving controller logic to User model

--- app/autocomplete_mention/controller.js
+++ app/autocomplete_mention/controller.js
@@ -1,17 +1,12 @@
 'use strict';
 
-var database = require('../../lib/mongo/');
+var User = require('./User');
 
-module.exports.mention = function mention(req,res) {
-  database.connect(function(err,db){
-    if(err) throw err;
-    var query = req.query.pattern;
-    db.collection('user').find({
-      name : {$regex:query}
-    }, {
-      name : 1 , _id : 0
-    }).toArray(function(err,matches) {
+module.exports.mention = (req,res) => {
+  var p = req.query.pattern;
+  User.findByPattern( p,
+    (err,matches) => {
+      if(err) throw err;
       res.send(matches);
     });
-  });
 };
