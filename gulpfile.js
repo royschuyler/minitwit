048e9d56eafb8a1b5a451fe827f30a1ae76894ea
Kyle Turco <kyleturco@gmail.com>
2015-08-24 13:48:25 -0500

    fix gulpfile to properly watch and compile

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,18 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var del = require('del');
 
-gulp.task('styles', function () {
-    gulp.src('www/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./www/styles'));
-});
-
-gulp.task('default',function () {
-  gulp.watch('www/**/*.scss',['start-clean']);
-});
-
-gulp.task('start-clean', function (cb) {
-  del('www/**/*.css', cb);
+gulp.task('watch',function () {
+  gulp.watch('www/**/*.scss',['clean', 'styles']);
 });
 
 gulp.task('clean', function () {
-  gulp.start('start-clean');
+  del('www/styles/main.css');
 });
+
+gulp.task('styles', function () {
+  gulp.src('www/**/*.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('./www/styles'));
+});
+
+gulp.task('default', ['styles']);
